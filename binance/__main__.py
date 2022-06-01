56d924488c0aef4c73dc9034e489fbb5cc8c142a
Eugene the Red <me@eugenethered.com>
2022-06-01 14:56:43 +0100

    update deps

--- binance/__main__.py
+++ binance/__main__.py
@@ -4,6 +4,7 @@ from cache.holder.RedisCacheHolder import RedisCacheHolder
 from config.report.holder.ConfigReporterHolder import ConfigReporterHolder
 from core.arguments.command_line_arguments import url_option_arg_parser
 from logger.ConfigureLogger import ConfigureLogger
+from metainfo.MetaInfo import MetaInfo
 
 from binance.BinanceExchangeDataStream import BinanceExchangeDataStream
 
@@ -11,7 +12,9 @@ from binance.BinanceExchangeDataStream import BinanceExchangeDataStream
 def start():
     ConfigureLogger()
 
-    command_line_arg_parser = url_option_arg_parser('persuader-technology-automata-exchange-data-stream-binance')
+    meta_info = MetaInfo('persuader-technology-automata-exchange-data-stream-binance')
+
+    command_line_arg_parser = url_option_arg_parser(meta_info)
     args = command_line_arg_parser.parse_args()
 
     log = logging.getLogger('Binance Exchange Data Stream')
